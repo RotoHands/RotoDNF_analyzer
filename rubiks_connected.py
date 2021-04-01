0cc88cd5f1d5337a989aa38a952834f3afc9b194
rotohands <rotohands@gmail.com>
2021-04-01 12:42:55 +0300

    updates

--- rubiks_connected.py
+++ rubiks_connected.py
@@ -2,7 +2,9 @@ from bleak import BleakClient
 import bleak
 import asyncio
 from bleak import BleakScanner
-
+async def print_test():
+    for i in range (1000):
+        print("1234")
 def parse_move(msgLen, value):
 
     axisPerm = [5, 2, 0, 3, 1, 4]
@@ -11,11 +13,13 @@ def parse_move(msgLen, value):
     curBatteryLevel = -1
     for i in range(0,msgLen,2):
 
+        if(msgLen == 10):
+            print(toHexVal(value))
         axis = axisPerm[value[3 + i] >> 1]
         power =[0, 2][value[3 + i] & 1]
         m = axis * 3 + power
-        print('move', "URFDLB"[axis] + " 2'"[power])
-
+        s = ("URFDLB"[axis] + " 2'"[power])
+        return (s)
 
 def parseData(value):
     if (len(value) < 4) :
@@ -25,7 +29,7 @@ def parseData(value):
     msgType = value[2]
     msgLen = len(value) - 6
     if (msgType == 1):
-        parse_move(msgLen, value)
+       return parse_move(msgLen, value)
 def toHexVal(value):
     valhex = []
     for i in range(len(value)):
@@ -34,43 +38,27 @@ def toHexVal(value):
     return valhex
 
 def callback(sender: int, data: bytearray):
-    data = parseData(data)
+    print(parseData(data))
 
 
 async def connect_to_device(address):
     print("starting", address, "loop")
     notify_uuid = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
+    write_uuid = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+    service = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
 
     async with BleakClient(address, timeout=15.0) as client:
         print("connect to", address)
 
         try:
+            print(await client.get_services())
+            await client.write_gatt_char(write_uuid, [51])
             await client.start_notify(notify_uuid, callback)
-            await client.start_notify()
-            await asyncio.sleep(30.0)
-            await client.stop_notify(notify_uuid)
+            await print_test()
         except Exception as e:
             print(e)
     print("disconnect from", address)
-async def main(addr):
-    addrs = "ec:6a:31:5b:17:2d"
-    addr = "DA:82:22:7A:A8:82"
-    UUID_SUFFIX = '-b5a3-f393-e0a9-e50e24dcca9e'
-    SERVICE_UUID = '6e400001' + UUID_SUFFIX
-    CHRCT_UUID_WRITE = '6e400002' + UUID_SUFFIX
-    CHRCT_UUID_READ = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
-    client = BleakClient(addr)
-    await client.pair()
-    print(await client.connect())
-    print("here")
-    await client.start_notify(CHRCT_UUID_READ, callback)
-    while True:
-        pass
-    # model_number = await client.read_gatt_char(CHRCT_UUID_READ)
-
-    # await client.start_notify()
-
-
-
+addr = "ec:6a:31:5b:17:2d"
+addrs = "DA:82:22:7A:A8:82"
 loop = asyncio.get_event_loop()
-loop.run_until_complete(connect_to_device("DA:82:22:7A:A8:82"))
+loop.run_until_complete(connect_to_device(addr))
