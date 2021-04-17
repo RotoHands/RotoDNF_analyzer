ec07f2a35f4a307851b80a2e5f2a7c862b319373
rotohands <rotohands@gmail.com>
2021-04-17 12:43:36 +0300

    gen url working

--- facelet_analyze.py
+++ facelet_analyze.py
@@ -2,7 +2,7 @@ import permutation
 import kociemba
 from RotoDNF import DNFanalyzer
 import re
-
+import pyperclip
 
 def ERROR_FUNC():
     print("unknown move: ")
@@ -21,10 +21,11 @@ class Cube:
         self.solved_edges = 0
         self.solved_corners = 0
         self.solved_perm = permutation.Permutation(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,54)
-
+        self.solve_stats = []
         self.current_perm = self.solved_perm
         self.scramble = ""
         self.solve = ""
+        self.url = ""
         self.current_facelet = ""
         self.R = permutation.Permutation(1, 2, 21, 4, 5, 24, 7, 8, 27, 16, 13, 10, 17, 14, 11, 18, 15, 12, 19, 20, 30, 22, 23, 33, 25, 26, 36, 28, 29, 52, 31, 32, 49, 34, 35, 46, 37, 38, 39, 40, 41,42, 43, 44, 45, 9, 47, 48, 6, 50, 51, 3, 53, 54).inverse()
         self.RP = self.R.inverse()
@@ -44,6 +45,8 @@ class Cube:
         self.F = permutation.Permutation(1, 2, 3, 4, 5, 6, 45, 42, 39, 7, 11, 12, 8, 14, 15, 9, 17, 18, 25, 22, 19, 26,23, 20, 27, 24, 21, 16, 13, 10, 31, 32, 33, 34, 35, 36, 37, 38, 28, 40, 41, 29,43, 44, 30, 46, 47, 48, 49, 50, 51, 52, 53, 54).inverse()
         self.FP = self.F.inverse()
         self.F2 = self.F * self.F
+
+
 
 
 
@@ -85,6 +88,24 @@ class Cube:
     def f2(self):
         self.current_perm = self.F * self.F * self.current_perm
 
+    def gen_url(self):
+        self.url = "https://www.cubedb.net/?rank=3&title={}&scramble=".format("test")
+        for move in self.scramble.split():
+            if "\'" in move:
+                move.replace("\'", "-")
+            self.url += "{}_".format(move)
+        self.url += "&alg="
+
+        for move in self.solve_stats:
+            if "\'" in move["move"]:
+                move["move"].replace("\'", "-")
+            self.url += "{}_".format(move["move"])
+            if move["comment"] != "":
+                self.url += move["comment"]
+        pyperclip.copy(self.url)
+
+
+
     def count_solved_cor(self):
         solved_corners = 0
 
@@ -92,22 +113,18 @@ class Cube:
             return 8
         for cor in self.corners_numbers:
             if str(cor) in self.current_perm_list:
-                print(str(cor))
                 solved_corners += 1
 
-        return 8 - solved_corners/3
+        return 8 - int(solved_corners/3)
 
     def count_solve_edges(self):
-        print(self.current_perm)
-        print(self.current_perm_list)
         solved_edges = 0
         if len(self.current_perm_list) <= 1:
             return 12
         for edge in self.edges_numbers:
             if str(edge) in self.current_perm_list:
-                print(str(edge))
                 solved_edges += 1
-        return 12 - solved_edges/2
+        return 12 - int(solved_edges/2)
 
     def singlemoveExecute(self, move):
         funcMoves = {
@@ -170,18 +187,33 @@ def main():
     SOLVED = "0UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB"
     MISTAKE_SOLVE_CORNERS = "D D U' R' U' D B2 U D' R' U D' D' U' D B' U D' R U' R' U' D B U2 D' U' R' L F R' L D2 R L' F R L' U D' F U' D R' U' R U D' F' U D R' L F R F B' D' D' F' B R F' L' R L' U L U' R' L F L' F' R R U2 R D R' U U R D' R' R' R U R D' R' U' R D R' R' R R R D D U' R'"
     SOLVE = "D U' D R' U' D B B U D' R' D' U D' U' D B' U D' R U' R' U' D B U D' U U' R' L F R' L D' D' R L' F R L' U D' F U' D R' U' R U D' F' U D R' L F R F B' D' D' F' B R F' L' R L' U L U' R' L F L' F' R R U U R D R' U2 R D' R' R' R U R D' R' U' R D R' R' R' U D' R' D R U' R' D' R D R D' L' D L D' L' D R' D' L D L' D' L D R"
+    SOLVE = "D U' D R' U' D B B U D' R' D' U D' U' D B' U D' R U' R' U' D B U D' U U' R' L F R' L D' D' R L' F R L' U D' F U' D R' U' R U D' F' U D R' L F R F B' D' D' F' B R F' L' R L' U L U' R' L F L' F' R R U U R D R' U2 R D' R' R' R U R D' R' U' R D R' R' R' U D' R' D R U' R' D' R D R D' L' D L D' L' D R' D' L D L' D' L D R"
     SCRAMBLE = "B2 F2 U2 F2 D' R2 U' L2 D L2 F2 B' R' U B2 R D' L B U'"
+    SCRAMBLE = "B2 F2 U2 F2 D R2 U' L2 D L2 F2 B' R' U B2 D R D' L B U'"
     cube = Cube()
     cube.scramble = SCRAMBLE
     cube.solve = SOLVE
     cube.current_facelet = SOLVED
     SCRAMBLE_LIST = SCRAMBLE.split()
+    count = -1
     for move in SCRAMBLE_LIST:
         cube.exe_move(move)
-    print(cube.current_perm)
-    print(cube.current_facelet)
+    max_solved = cube.count_solved_cor() + cube.count_solve_edges()
+    for move in cube.solve.split():
+        count += 1
+        cube.exe_move(move)
+        solved_edges =  cube.count_solve_edges()
+        solved_cor = cube.count_solved_cor()
+        solved = solved_cor + solved_edges
+        if solved > max_solved:
+            max_solved = solved
+            cube.solve_stats.append({"count" : count,"move": move, "ed" : solved_edges,"cor" :  solved_cor, "comment" : "//e : {}, c : {}%0A".format(solved_edges, solved_cor)})
+        else:
+            cube.solve_stats.append({"count" : count,"move": move, "ed" : solved_edges,"cor" :  solved_cor, "comment" : ""})
+
     print("corners : {}, edges : {}".format(cube.count_solved_cor(), cube.count_solve_edges()))
     print(kociemba.solve(cube.current_facelet[1:], SOLVED[1:]))
-
+    cube.gen_url()
+    print(*cube.solve_stats, sep="\n")
 if __name__ == '__main__':
     main()
